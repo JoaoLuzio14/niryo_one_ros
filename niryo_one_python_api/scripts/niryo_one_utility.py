f87db13e6f33ef23948b6401c5ae11aed492e5ad
João Miguel Barradas Luzio <54044365+JoaoLuzio14@users.noreply.github.com>
2021-12-16 23:51:21 +0000

    Added Forward Kinematics Calculator

---
+++ niryo_one_python_api/scripts/niryo_one_utility.py
@@ -0,0 +1,56 @@
+# Niryo One utility Functions
+import numpy as np
+from numpy.linalg import inv
+import math
+
+
+def ForwardKinematics(t1, t2, t3, t4, t5, t6):
+	# Rotation around Z axis
+	tf_01 = np.array([[ math.cos(t1), -math.sin(t1),    0,   0],
+					  [ math.sin(t1),  math.cos(t1),    0,   0],
+					  [            0,             0,    1, 103],
+					  [            0,             0,    0,   1]])
+	# Rotation around X axis
+	tf_12 = np.array([[  math.cos(t2),   0, math.sin(t2),   0],
+					  [             0,   1,            0,   0],
+					  [ -math.sin(t2),   0, math.cos(t2),  80],
+					  [             0,   0,            0,   1]])
+	# Rotation around Y axis
+	tf_23 = np.array([[  math.cos(t3),   0, math.sin(t3),   0],
+					  [             0,   1,            0,   0],
+					  [ -math.sin(t3),   0, math.cos(t3), 210],
+					  [             0,   0,            0,   1]])
+	# Rotation around X axis
+	tf_34 = np.array([[   1,             0,             0,  41.5],
+					  [   0,  math.cos(t4), -math.sin(t4),     0],
+					  [   0,  math.sin(t4),  math.cos(t4),    30],
+					  [   0,             0,             0,     1]])
+	# Rotation around Y axis
+	tf_45 = np.array([[  math.cos(t5),    0, math.sin(t5),   180],
+					  [             0,    1,            0,     0],
+					  [ -math.sin(t5),    0, math.cos(t5),     0],
+					  [             0,    0,            0,     1]])
+	# Rotation around X axis
+	tf_56 = np.array([[   1,             0,             0,  23.7],
+					  [   0,  math.cos(t6), -math.sin(t6),     0],
+					  [   0,  math.sin(t6),  math.cos(t6),  -5.5],
+					  [   0,             0,             0,     1]])
+
+
+	tf_46 = np.dot(tf_45, tf_56)
+	#print(tf_46)
+	tf_36 = np.dot(tf_34, tf_46)
+	#print(tf_36)
+	tf_26 = np.dot(tf_23, tf_36)
+	#print(tf_26)
+	tf_16 = np.dot(tf_12, tf_26)
+	#print(tf_16)
+	tf_06 = np.dot(tf_01, tf_16)
+	#print(tf_06)
+
+	return tf_06
+
+
+
+
+	
