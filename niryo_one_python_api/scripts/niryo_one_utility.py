b628228369995086f9890fe25af953c67909c043
João Miguel Barradas Luzio <54044365+JoaoLuzio14@users.noreply.github.com>
2021-12-18 10:35:16 +0000

    ForwardKinematics now functional

--- niryo_one_python_api/scripts/niryo_one_utility.py
+++ niryo_one_python_api/scripts/niryo_one_utility.py
@@ -10,16 +10,16 @@ def ForwardKinematics(t1, t2, t3, t4, t5, t6):
 					  [ math.sin(t1),  math.cos(t1),    0,   0],
 					  [            0,             0,    1, 103],
 					  [            0,             0,    0,   1]])
-	# Rotation around X axis
-	tf_12 = np.array([[  math.cos(t2),   0, math.sin(t2),   0],
-					  [             0,   1,            0,   0],
-					  [ -math.sin(t2),   0, math.cos(t2),  80],
-					  [             0,   0,            0,   1]])
 	# Rotation around Y axis
-	tf_23 = np.array([[  math.cos(t3),   0, math.sin(t3),   0],
-					  [             0,   1,            0,   0],
-					  [ -math.sin(t3),   0, math.cos(t3), 210],
-					  [             0,   0,            0,   1]])
+	tf_12 = np.array([[ math.cos(t2),   0, -math.sin(t2),   0],
+					  [            0,   1,             0,   0],
+					  [ math.sin(t2),   0,  math.cos(t2),  80],
+					  [            0,   0,             0,   1]])
+	# Rotation around Y axis
+	tf_23 = np.array([[ math.cos(t3),   0, -math.sin(t3),   0],
+					  [            0,   1,             0,   0],
+					  [ math.sin(t3),   0,  math.cos(t3), 210],
+					  [            0,   0,             0,   1]])
 	# Rotation around X axis
 	tf_34 = np.array([[   1,             0,             0,  41.5],
 					  [   0,  math.cos(t4), -math.sin(t4),     0],
@@ -37,24 +37,18 @@ def ForwardKinematics(t1, t2, t3, t4, t5, t6):
 					  [   0,             0,             0,     1]])
 
 
-	tf_46 = np.dot(tf_45, tf_56)
+	tf_46 = np.matmul(tf_45, tf_56)
 	#print(tf_46)
-	tf_36 = np.dot(tf_34, tf_46)
+	tf_36 = np.matmul(tf_34, tf_46)
 	#print(tf_36)
-	tf_26 = np.dot(tf_23, tf_36)
+	tf_26 = np.matmul(tf_23, tf_36)
 	#print(tf_26)
-	tf_16 = np.dot(tf_12, tf_26)
+	tf_16 = np.matmul(tf_12, tf_26)
 	#print(tf_16)
-	tf_06 = np.dot(tf_01, tf_16)
+	tf_06 = np.matmul(tf_01, tf_16)
 	#print(tf_06)
 
 	position = np.dot(tf_06, np.transpose(np.array([0, 0, 0, 1])))
 	position = position[0:3]
 
 	return position
-
-
-
-
-
-	
